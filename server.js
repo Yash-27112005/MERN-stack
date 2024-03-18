b4a4a492714fa26e02e3abb25e77fe523597f3bc
Yash Stpute <161969120+Yash-27112005@users.noreply.github.com>
2024-03-18 10:23:24 +0530

    and db.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
-
+const connectDb = require("./utils/db.js");
 app.use(express.json());
 
-app.use("/api/auth",router);
+app.use("/api/auth", router);
 
 const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`run on ${PORT}`);
+
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`run on ${PORT}`);
+  });
 });
---
+++ utils/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const URI = "mongodb://127.0.0.1:27017/mern_admin";
+// mongoose.connect(URL);
+const connectDb = async () => {
+  try {
+    await mongoose.connect(URI);
+    console.log("connection successful yo DB");
+  } catch (error) {
+    console.error("database connection failed");
+    process.exit(0);
+  }
+};
+
+module.exports = connectDb;
