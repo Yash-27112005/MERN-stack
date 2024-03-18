bbb22418da9dab2fd979e21790fb36e08cbf617e
Yash Stpute <161969120+Yash-27112005@users.noreply.github.com>
2024-03-19 01:15:39 +0530

    add (give error if email is already exists)

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,3 +1,6 @@
+const { json } = require("express");
+const User = require("../models/user-model");
+
 const home = async (req, res) => {
   try {
     res.status(200).send("hi im yash in router / controller");
@@ -8,12 +11,19 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    console.log(req.body);
-    res.status(200).json({
-      message: req.body,
-    });
+    // console.log(req.body);
+    const { username, email, phone, password } = req.body;
+
+    const userExist = await User.findOne({email});
+
+    if (userExist) {
+      return res.status(400).json({ msg: "email already exists" });
+    }
+    const userCreated = await User.create({ username, email, phone, password });
+
+    res.status(200).json({ msg: userCreated });
   } catch (error) {
-    res.status(400).json({ msg: "Page not found" });
+    res.status(500).json("Internal server error" );
   }
 };
 module.exports = { home, register };
