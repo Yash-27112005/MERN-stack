8a6893fc38b750fb43eec10a327aeb46bf50f5ea
Yash Stpute <161969120+Yash-27112005@users.noreply.github.com>
2024-03-20 00:24:01 +0530

    add contact

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
+const contactRouter = require("./router/contact-router.js");
 const connectDb = require("./utils/db.js");
 const errorMiddleware = require("./middlewares/error-middleware.js");
 
 app.use(express.json());
 
 app.use("/api/auth", router);
-
+app.use("/api/form", contactRouter);
 app.use(errorMiddleware);
 
 const PORT = 5000;
