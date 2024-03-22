7a27f4293aab03706d1a02d219166c8339cdd9a5
Yash Stpute <161969120+Yash-27112005@users.noreply.github.com>
2024-03-23 01:23:30 +0530

    add Backend to Registration and add CORS

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const router = require("./router/auth-router");
 const contactRouter = require("./router/contact-router.js");
 const connectDb = require("./utils/db.js");
 const errorMiddleware = require("./middlewares/error-middleware.js");
+
+// let's add cors
+
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentials:true,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
