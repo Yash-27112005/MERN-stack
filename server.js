fb9413f2efa561b82c9408999c99558fe858f3d6
Yash Stpute <161969120+Yash-27112005@users.noreply.github.com>
2024-03-26 23:12:55 +0530

    add service data to backend

--- server.js
+++ server.js
@@ -2,25 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const router = require("./router/auth-router");
+const authRouter = require("./router/auth-router");
 const contactRouter = require("./router/contact-router.js");
+const serviceRouter = require("./router/service-router.js");
 const connectDb = require("./utils/db.js");
 const errorMiddleware = require("./middlewares/error-middleware.js");
 
 // let's add cors
 
-
 const corsOptions = {
   origin: "http://localhost:5173",
-  methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials:true,
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use("/api/auth", router);
+app.use("/api/auth", authRouter);
 app.use("/api/form", contactRouter);
+app.use("/api/data", serviceRouter);
 app.use(errorMiddleware);
 
 const PORT = 5000;
