85b6f08b5d1b948995cbd2ee4e3b13b4efa65974
Yash Stpute <161969120+Yash-27112005@users.noreply.github.com>
2024-03-29 00:21:34 +0530

    get Contact And User If login for Admin

--- router/admin-router.js
+++ router/admin-router.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const adminController = require("../controllers/admin-controllers");
+const authMiddleware = require("../middlewares/auth-Middleware");
+
 const router = express.Router();
 
-router.route("/users").get(adminController.getAllUsers);
-router.route("/contacts").get(adminController.getAllContacts);
+router.route("/users").get(authMiddleware,adminController.getAllUsers);
+router.route("/contacts").get(authMiddleware,adminController.getAllContacts);
+
 module.exports = router;
